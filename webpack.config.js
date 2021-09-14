104bea4b784192094dd15bd05c552c4dc1a47fe7
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-09-14 21:59:58 +0300

    all works

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,56 @@
 const path = require("path");
 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "my-webppack-bundle.js",
+    path: path.resolve("./build"),
+    filename: "main-webpack.bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.js$/i,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.hbs$/i,
+        loader: "handlebars-loader",
+      },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|webp)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "src/index.html" }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    client: {
+      logging: "error",
+    },
+    port: 1234,
+    allowedHosts: "all",
+    open: true,
   },
 };
